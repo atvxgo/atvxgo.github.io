5d1a0e75622bbd0a7a0910b91203c6655c2da3a9
atvxgo <134418693+atvxgo@users.noreply.github.com>
2025-02-08 10:21:51 +0100

    Add files via upload

--- urls.js
+++ urls.js
@@ -1,12 +1,9 @@
 var urls = [
-    'img/bgblr/xmas/1.jpg',
-    'img/bgblr/xmas/2.jpg',
-    'img/bgblr/xmas/3.jpg',
-    'img/bgblr/xmas/4.jpg',
-    'img/bgblr/xmas/5.jpg',
-    'img/bgblr/xmas/6.jpg',
+    'img/bgblr/1.jpg',
+    'img/bgblr/2.jpg',
     'img/bgblr/3.jpg',
     'img/bgblr/4.jpg',
-    'img/bgblr/8.jpg',
-    'img/bgblr/10.jpg'
+    'img/bgblr/5.jpg',
+    'img/bgblr/6.jpg',
+    'img/bgblr/7.jpg',
 ];
